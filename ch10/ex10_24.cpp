dab0dcf653eca05abddeb6faf40139367ab75210
pengxin123 <pengxinliannan@hotmail.com>
2016-01-15 00:13:06 -0500

    Update ex10_24.cpp

--- ch10/ex10_24.cpp
+++ ch10/ex10_24.cpp
@@ -1,12 +1,9 @@
 //
-// @author @Yue Wang @shbling @Soyn @Yue Wang
+// @author Xin Peng
 //
 // Exercise 10.24:
 // Use bind and check_size to find the first element in a vector of ints that has a value greater
 // than the length of a specified string value.
-//
-//  Discussion over this exercise on StackOverflow
-//  http://stackoverflow.com/questions/20539406/what-type-does-stdfind-if-not-return
 //
 
 #include <iostream>
@@ -19,29 +16,25 @@ using std::endl;
 using std::string;
 using std::vector;
 using std::find_if;
-using std::bind;
-using std::size_t;
+using namespace std::placeholders;
 
-auto check_size(string const& str, size_t sz)
+bool check_size(const string &s, string::size_type sz)
 {
-    return str.size() < sz;
+    return s.size() < sz;
 }
-
-auto find_first_greater(vector<int> const& v, string const& str)
+vector<int>::iterator find_first_greater( vector<int> & v, const string &str)
 {
-    auto predicate = [&](int i){ return bind(check_size, str, i)(); };
-    return find_if(v.cbegin(), v.cend(), predicate);
+     return find_if(v.begin(),v.end(),bind(check_size, str, _1));
 }
 
 int main()
 {
     vector<int> vec{ 0, 1, 2, 3, 4, 5, 6, 7 };
-    string str("123456");
+    string str("abcdef");
     auto result = find_first_greater(vec, str);
     if (result != vec.cend())
         cout << *result << endl;
     else
         cout << "Not found" << endl;
-
     return 0;
-}
+}  
